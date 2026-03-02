27d37cf703a84a691a7ff647dc39152d6ecdcba0
Thenuk Rodrigo <thenuk.rodrigo@outlook.com>
2026-03-02 06:16:11 -0800

    positional embeddings added

---
+++ positional_embedding.py
@@ -0,0 +1,18 @@
+import torch 
+from config import * 
+
+class PositionalEmbeddings(nn.Module): 
+    def __init__(self):
+        super().__init__() 
+        self.max_seq_len = max_seq_len
+        self.dim = action_dim
+
+        self.pos_embed = nn.Parameter(torch.randn(max_seq_len, action_dim)) * 0.02 
+
+        
+    def forward(self, tokens):
+        # tokens: (B, length + 1, action_dim)
+        seq_len = tokens.shape[1]
+        pos = self.pos_embed[:seq_len, :]
+    
+        return tokens + pos 
